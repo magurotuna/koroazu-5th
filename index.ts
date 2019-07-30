44d478403e532ed59dab2e72fffcbd91070d3e9b
Yusuke Tanaka <wing0920@gmail.com>
2019-07-31 05:13:58 +0900

    デバッグ用の記述を削除

--- index.ts
+++ index.ts
@@ -2,13 +2,10 @@ import * as path from 'path'
 import { fetchTweets } from './lib/fetch-tweets'
 import { count } from './lib/count'
 
-
 const main = async () => {
-  // const tweets = await fetchTweets()
-
+  const tweets = await fetchTweets()
   const songsYamlPath = path.resolve(path.join(__dirname, 'lib', 'songs.yml'))
-  const tweetsJsonPath = path.resolve(path.join(__dirname, 'output', 'tweets_2019-7-31_3-57.json'))
-  const counter = count(songsYamlPath, tweetsJsonPath)
+  const counter = count(songsYamlPath, tweets)
   console.log(counter)
 }
 
