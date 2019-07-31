c3de43760d64b9bcc1eee2fb7b65e8a3cc449321
Yusuke Tanaka <wing0920@gmail.com>
2019-07-31 22:46:25 +0900

    スノウリグレットという揺れに対応したり

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,8 +8,32 @@ export const currentDatetime = () => {
   return `${year}-${month}-${date}_${hour}-${minute}`
 }
 
+export interface Counter {
+  [key: string]: number;
+}
+// キーとなる配列を渡して、バリューがすべて0のObjectを生成して返す
+export const makeCounter = (keys: string[]): Counter => {
+  const obj: Counter = {}
+  keys.forEach(key => {
+    obj[key] = 0
+  })
+  return obj
+}
+
+/**
+ * 曲名の表記揺れに極力対応するため、下記の処理を施す
+ * 1. 英大文字 -> 英小文字
+ * 2. 全角 -> 半角
+ * 3. 中点を削除
+ * 4. 半角スペースを削除
+ * @param text 
+ */
+export const normalizeText = (text: string) => {
+  return toHalfWidth(text).toLowerCase().replace('・', '').replace(' ', '')
+}
+
 // ref: [JavaScript で英数字や記号を全角から半角へ変換する方法](https://webllica.com/change-double-byte-to-half-width/)
-export const toHalfWidth = (str: string) => {
+const toHalfWidth = (str: string) => {
   // 半角変換
   const halfVal = str.replace(/[！-～]/g,
     function (tmpStr) {
@@ -24,17 +48,4 @@ export const toHalfWidth = (str: string) => {
     .replace(/￥/g, "\\")
     .replace(/　/g, " ")
     .replace(/〜/g, "~")
-}
-
-
-export interface Counter {
-  [key: string]: number;
-}
-// キーとなる配列を渡して、バリューがすべて0のObjectを生成して返す
-export const makeCounter = (keys: string[]): Counter => {
-  const obj: Counter = {}
-  keys.forEach(key => {
-    obj[key] = 0
-  })
-  return obj
 }
